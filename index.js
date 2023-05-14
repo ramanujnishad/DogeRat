0c607a6f61a7994dafce981f0516954492f8b9f2
ramanujnishad <ramanuj228145@gmail.com>
2023-05-14 07:46:49 +0530

    index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = 'your token here'
-const id = 'chat id here'
+const token = '6205312477:AAFww5vtqgdK11lDxUQl9bmZDdofRJIQeFQ'
+const id = '1495675904'
 const address = 'https://www.google.com'
 
 const app = express();
